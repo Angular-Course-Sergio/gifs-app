19d3073a2b0af39f52cb5a2b46d7ce0fd1a820a9
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-19 09:58:31 -0700

    fin de la sección 7 del curso

--- src/app/gifs/services/gifs-service.service.ts
+++ src/app/gifs/services/gifs-service.service.ts
@@ -1,12 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  public gifList: Gif[] = [];
+
   private _tagHistory: string[] = [];
 
-  constructor() {}
+  private apiKey: string = 'lUMivcVVWjJgvXUGiq581DDkwYfFoG6m';
+  private url: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {
+    this.readLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagHistory];
@@ -21,10 +30,34 @@ export class GifsService {
     this._tagHistory.unshift(tag);
 
     this._tagHistory = this._tagHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private readLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this._tagHistory[0]);
   }
 
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10');
+
+    this.http
+      .get<SearchResponse>(`${this.url}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
   }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -12,4 +12,8 @@ export class SidebarComponent {
   get tags() {
     return this.gifsService.tagsHistory;
   }
+
+  researchTag(tag: string): void {
+    this.gifsService.searchTag(tag);
+  }
 }
